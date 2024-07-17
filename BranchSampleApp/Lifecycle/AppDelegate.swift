bcfd4398162069addfbd2c7389981c5f68215aff
Arsalan <mdarsalan.ahmad@branch.io>
2024-07-17 21:09:09 +0530

    gdpr

--- BranchSampleApp/Lifecycle/AppDelegate.swift
+++ BranchSampleApp/Lifecycle/AppDelegate.swift
@@ -21,6 +21,8 @@ final class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationC
         return AppCoordinator(navigationController: navigationController)
     }()
     
+    var firstOpen = true
+    
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -31,9 +33,37 @@ final class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationC
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         
-        
         Branch.enableLogging()
+        Branch.setTrackingDisabled(true)
         
+        if !firstOpen {
+            initaliseBranch(didFinishLaunchingWithOptions: launchOptions)
+        }else{
+            let alert = UIAlertController(title: "Tracking", message: "Do you want to allow tracking", preferredStyle: .alert)
+
+            let actionYes = UIAlertAction(title: "Yes", style: .default, handler: { action in
+                Branch.setTrackingDisabled(false)
+                self.initaliseBranch(didFinishLaunchingWithOptions: launchOptions)
+            })
+
+            let actionCancel = UIAlertAction(title: "No", style: .destructive, handler: { action in
+                Branch.setTrackingDisabled(true)
+                self.initaliseBranch(didFinishLaunchingWithOptions: launchOptions)
+            })
+
+            alert.addAction(actionYes)
+            alert.addAction(actionCancel)
+
+            DispatchQueue.main.async {
+                self.window?.rootViewController?.present(alert, animated: true, completion: nil)
+            }
+        }
+        
+        
+        return true
+    }
+    
+    func initaliseBranch( didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Void {
         Branch.getInstance().checkPasteboardOnInstall()
         
         Branch.getInstance().initSession(launchOptions: launchOptions) { (params, error) in
@@ -45,14 +75,14 @@ final class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationC
                 self.appCoordinator.push(view: MovieDetailView(id : Int(data["nav_id"] as? String ?? "") ?? 0).environmentObject(movies))
             }
         }
-        
-        return true
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         Branch.getInstance().application(app, open: url, options: options)
         return true
     }
+    
+    
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
           // Handler for Universal Links
